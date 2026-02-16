939ea46b3816d451fd438c4d37d33f99a2b79324
letquit <1446444589@qq.com>
2026-02-16 09:43:21 +0800

    Find Target and Shooting

--- Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
+++ Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
@@ -83,7 +83,7 @@ public class UnitSelectionManager : MonoBehaviour
                 PhysicsWorldSingleton physicsWorldSingleton = entityQuery.GetSingleton<PhysicsWorldSingleton>();
                 CollisionWorld collisionWorld = physicsWorldSingleton.CollisionWorld;
                 UnityEngine.Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                int unitsLayer = 6;
+                
                 RaycastInput raycastInput = new RaycastInput
                 {
                     Start = cameraRay.GetPoint(0f),
@@ -91,13 +91,13 @@ public class UnitSelectionManager : MonoBehaviour
                     Filter = new CollisionFilter
                     {
                         BelongsTo = ~0u,
-                        CollidesWith = 1u << unitsLayer,
+                        CollidesWith = 1u << GameAssets.UNITS_LAYER,
                         GroupIndex = 0,
                     },
                 };
                 if (collisionWorld.CastRay(raycastInput, out Unity.Physics.RaycastHit raycastHit))
                 {
-                    if (entityManager.HasComponent<Unit>(raycastHit.Entity))
+                    if (entityManager.HasComponent<Unit>(raycastHit.Entity) && entityManager.HasComponent<Selected>(raycastHit.Entity))
                     {
                         // Hit a Unit
                         entityManager.SetComponentEnabled<Selected>(raycastHit.Entity, true);
